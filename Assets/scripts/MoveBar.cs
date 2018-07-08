feaee729845598c8e6c23016b7a2aaaf09af7596
Chris Yin <chrisyfrisky@gmail.com>
2018-07-08 02:21:36 -0700

    Implement "Press Space to continue" prompt
    It appears after all the text is done scrolling in the title screen.
    Currently it prints that space was pressed to the console, but it will
    be programmed to go to the title screen proper of the game.

--- Assets/scripts/MoveBar.cs
+++ Assets/scripts/MoveBar.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class MoveBar : MonoBehaviour {
 
-	private bool moving = false;
+	public bool moving = false;
 
 	void Update () {
 		if (moving) {
--- Assets/scripts/TitleScreen.cs
+++ Assets/scripts/TitleScreen.cs
@@ -1,20 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class TitleScreen : MonoBehaviour {
 
 	public GameObject[] bars;
+	public GameObject pressSpaceBar;
 
 	private float timer;
 	private int index;
+	private Renderer pressSpaceBarRenderer;
 
 	// Use this for initialization
 	void Start () {
 		timer = 0f;
 		index = 0;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (index < bars.Length) {
@@ -23,6 +26,11 @@ public class TitleScreen : MonoBehaviour {
 				index++;
 			}
 			timer += Time.deltaTime;
+		} else if (bars.All(x => !x.GetComponent<MoveBar>().moving)) { // All bars moved
+			pressSpaceBarRenderer = pressSpaceBar.GetComponent<Renderer>();
+			pressSpaceBarRenderer.enabled = true;
+			if (Input.GetKeyDown("space"))
+				print("Space pressed");
 		}
 	}
 }
